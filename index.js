32387fde4370fe116af9ce89625217aedb608b8a
Caroline kahuro <carolkahuro@gmail.com>
2023-07-26 13:09:26 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const idElement = document.getElementById('main');
+idElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+const body1 = document.querySelector('body');
+body1.appendChild(newHeader);
+document.getElementsByTagName('h1')[0].textContent='Kahuro is the champion';
